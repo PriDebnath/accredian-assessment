2635293b4ca9ba71c3b3607862ac40221d8495f3
Pritam <debnathpritam0802@gmail.com>
2026-07-30 16:51:15 +0530

    fixing cases

--- feature/landing-page/component/layout/langing-page-layout.tsx
+++ feature/landing-page/component/layout/langing-page-layout.tsx
@@ -1,4 +1,4 @@
-import Navbar from "@/feature/landing-page/component/navbar/index";
+import Navbar from "@/feature/landing-page/component/temp-navbar/index";
 
 export default function LangingPageLayout({
   children,
